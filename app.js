1ff1872b4d70767415c4e8ab7873951e19a43588
foxsexy198 <lankc3939@gmail.com>
2023-10-06 16:49:52 +0700

    Cai dat cac handler truy xuat CSDL

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"))
+});
+
+
+app.use((ApiError, req, res, next) => {
+    return res.status(ApiError.statusCode || 500).json({
+        message: ApiError.message || "Internal Server Error"
+    });
+});
+
+
+
+module.exports = app;
